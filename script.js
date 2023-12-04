9f134c8485178b9257af42a5ebf0224e9eef41a7
Jubair02 <104502849+Jubair02@users.noreply.github.com>
2023-12-05 01:41:55 +0600

    commited

--- script.js
+++ script.js
@@ -3,12 +3,28 @@ const quoteText =document.getElementById('quote');
 const authorText =document.getElementById('author');
 const twitterBtn =document.getElementById('twitter');
 const newQuoteBtn =document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 
 
 let apiQuotes = [];
+//Show Loading
+const loading= ()=>{
+    loader.hidden=false;
+    quoteContainer.hidden=true
+}
+
+// hide Loading
+const complete= ()=>{
+    loader.hidden=true;
+    quoteContainer.hidden=false;
+}
+
+
+
 //Show New Quote
 const newquote= () =>{
+    loading();
     const quote=apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
     //Check if Author field is blank and replace it with 'unknown'
@@ -27,15 +43,15 @@ const newquote= () =>{
         }
 
     
-
-
+// Set Quote, Hide Loader
     quoteText.textContent=quote.text;
-   
+    complete();   
     
 }
 
 //Get quotes from API
 async function getquotes(){
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch (apiUrl);
@@ -61,3 +77,4 @@ newQuoteBtn.addEventListener('click',newquote)
 
 // On load 
 getquotes();
+
